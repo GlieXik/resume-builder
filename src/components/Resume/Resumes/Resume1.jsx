577fd6074570c80673967f05790bf335cf97264e
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-10-02 19:36:17 +0300

    +-

--- src/components/Resume/Resumes/Resume1.jsx
+++ src/components/Resume/Resumes/Resume1.jsx
@@ -7,7 +7,7 @@ import {
   Link,
   PDFDownloadLink,
 } from "@react-pdf/renderer";
-import { PDFViewer } from "@react-pdf/renderer";
+
 import { useSelector } from "react-redux";
 import { getSkills, getData } from "../../../redux/selectors";
 
--- src/components/Steper/Steps/Second/Second.jsx
+++ src/components/Steper/Steps/Second/Second.jsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+// import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
